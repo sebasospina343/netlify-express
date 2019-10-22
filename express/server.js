2850ccfbd9924a8fdea7aab854fdc3b582b1b1d4
Sebastian Ospina <sebasospina343@gmail.com>
2019-10-22 16:35:01 -0500

    changed server2

--- express/server.js
+++ express/server.js
@@ -7,9 +7,44 @@ const bodyParser = require('body-parser')
 
 const router = express.Router()
 router.get('/', (req, res) => {
-  res.json({
-    test: 'test'
-  })
+  res.json([
+    {
+      customerId: '59767559181262060060278870901087098211',
+      email: 'pifdemo1@putitforward.com',
+      firstName: 'John',
+      lastName: 'Smith'
+    },
+    {
+      customerId: '60767559181262060060278870901087098223',
+      email: 'pifdemo2@putitforward.com',
+      firstName: 'Sandra',
+      lastName: 'Jones'
+    },
+    {
+      customerId: '10767559181262060060278870901087098234',
+      email: 'pifdemo3@putitforward.com',
+      firstName: 'Rebecca',
+      lastName: 'Thomson'
+    },
+    {
+      customerId: '72767559181262060060278870901087098245',
+      email: 'pifdemo4@putitforward.com',
+      firstName: 'Johannes',
+      lastName: 'Deckers'
+    },
+    {
+      customerId: '30767559181262060060278870901087098256',
+      email: 'pifdemo4@putitforward.com',
+      firstName: 'Milton',
+      lastName: 'Houst'
+    },
+    {
+      customerId: '40767559181262060060278870901087098267',
+      email: 'pifdemo4@putitforward.com',
+      firstName: 'Oscar',
+      lastName: 'Mendel'
+    }
+  ])
 })
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }))
 router.post('/', (req, res) => res.json({ postBody: req.body }))
